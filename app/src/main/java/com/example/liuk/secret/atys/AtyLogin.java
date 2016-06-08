bb5f6583fb420add250a7aa2be71eb1fad862253
liukqsmy <liukqsmy@sina.com>
2016-06-08 09:24:46 +0800

    登陆界面

--- app/src/main/java/com/example/liuk/secret/atys/AtyLogin.java
+++ app/src/main/java/com/example/liuk/secret/atys/AtyLogin.java
@@ -11,5 +11,7 @@ public class AtyLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aty_login);
+
+
     }
 }
