ab9519d0e492b7975592fdddc425cf8d4c5aabed
liukqsmy <liukqsmy@sina.com>
2016-06-13 16:08:11 +0800

    9jiang

--- app/src/main/java/com/example/liuk/secret/net/NetConnection.java
+++ app/src/main/java/com/example/liuk/secret/net/NetConnection.java
@@ -39,7 +39,7 @@ public class NetConnection {
                             bw.write(paramStr.toString());
                             break;
                         default:
-                            uc = new
+                            //uc = new
                             break;
 
                     }
