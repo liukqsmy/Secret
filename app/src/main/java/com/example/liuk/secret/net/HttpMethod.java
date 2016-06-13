211da4086e412b866bbf902c71d74fe4ca66720b
liukqsmy <liukqsmy@sina.com>
2016-06-13 09:23:30 +0800

    第九集

---
+++ app/src/main/java/com/example/liuk/secret/net/HttpMethod.java
@@ -0,0 +1,7 @@
+package com.example.liuk.secret.net;
+
+/**
+ * Created by Administrator on 2016/6/13.
+ */
+public class HttpMethod {
+}
--- app/src/main/java/com/example/liuk/secret/net/NetConnection.java
+++ app/src/main/java/com/example/liuk/secret/net/NetConnection.java
@@ -4,4 +4,7 @@ package com.example.liuk.secret.net;
  * Created by Administrator on 2016/6/7.
  */
 public class NetConnection {
+    public NetConnection(String url, HttpMethod method, String... kvs){
+
+    }
 }
