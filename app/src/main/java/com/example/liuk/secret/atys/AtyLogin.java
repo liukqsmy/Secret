02dbb9f54ab85383540dc21fdf4615d471ce11ea
liukqsmy <liukqsmy@sina.com>
2016-06-16 09:23:13 +0800

    登录界面设计

--- app/src/main/java/com/example/liuk/secret/atys/AtyLogin.java
+++ app/src/main/java/com/example/liuk/secret/atys/AtyLogin.java
@@ -1,9 +1,15 @@
 package com.example.liuk.secret.atys;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.liuk.secret.R;
+import com.example.liuk.secret.net.GetCode;
 
 public class AtyLogin extends AppCompatActivity {
 
@@ -12,6 +18,38 @@ public class AtyLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aty_login);
 
+        etPhone = (EditText)findViewById(R.id.etPhoneNum);
+
+        findViewById(R.id.btnGetCode).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(etPhone.getText())){
+                    Toast.makeText(AtyLogin.this, R.string.phone_num_cannot_be_empty, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                final ProgressDialog pd = ProgressDialog.show(AtyLogin.this, getResources().getString(R.string.connecting), getResources().getString(R.string.connecting_to_server));
+                new GetCode(etPhone.getText().toString(), new GetCode.SuccessCallback(){
+
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(AtyLogin.this, R.string.success_to_get_code, Toast.LENGTH_LONG).show();
+
+                    }
+                },new GetCode.FailCallback(){
+
+                    @Override
+                    public void onFail() {
+                        Toast.makeText(AtyLogin.this, R.string.fail_to_get_code, Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+
+
+        });
 
     }
+
+    private EditText etPhone = null;
 }
